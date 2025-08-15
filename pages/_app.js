9caa868626340357fa11b19f6636409d53731416
niceyy-dev <zhanais.dev@gmail.com>
2025-08-15 16:43:58 +0900

    wallet ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
 import "../styles/globals.css";
+import { config } from "../provider/wagmiConfigs";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { QueryClientProvider, QueryClient, Query } from "@tanstack/react-query";
+import { Web3Provider } from "../context/Web3Provider";
+import { ToastProvider } from "../context/ToastContext";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: "D345EF",
+            accentColorForeground: "white",
+            borderRadius: "snall",
+            fontStack: "system",
+            overlayBlur: "small",
+          })}
+        >
+          <ToastProvider>
+            <Web3Provider>
+              <Component {...pageProps} />
+            </Web3Provider>
+          </ToastProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
 
 export default MyApp;
---
+++ provider/hooks.js
@@ -0,0 +1,43 @@
+import { providers } from "ethers";
+import { useMemo } from "react";
+import { useClient, useConnectorClient } from "wagmi";
+
+export function clientToProvider(client) {
+  const { chain, transport } = client;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddresss: chain.contracts?.ensRegistry?.address,
+  };
+  if (transport.type === "fallback") {
+    return new providers.FallbackProvider(
+      transport.transports.map(
+        ({value })= new providers.JsonRpcProvider(value?.url, network)
+      )
+    );
+  }
+  return new providers.JsonRpcProvider(transport.url, network)
+}
+
+
+export function useEthersProvider({ chainId } = {}){
+    const client = useClient({chainId});
+    return useMemo(() => (client ? clientToProvider(client) : undefined), [client]);
+}
+
+export function clientToSigner(client){
+    const {account, chain, transport } = client;
+    const network = {
+        chainId: chain.id,
+        name: chain.name,
+        ensAddresss: chain.contracts?.ensRegistry?.address,
+    };
+    const provider = new provider.Web3Provider(transport, network);
+    const signer = provider.getSigner(account);
+    return signer;
+}
+
+export function useEthersSigner({ chainId } = {}){
+    const client = useConnectorClient({chainId});
+    return useMemo(() => (client ? clientToSigner(client) : undefined), [client]);
+}
